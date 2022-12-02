9c9fef35c8580203d8024d750b2ab14bdabaff23
Dilip Thomas <dthomas84@gmail.com>
2022-12-02 13:10:21 +1100

    day 1 exercise.

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,7 +1,11 @@
-import { hello } from './day1';
+import { maxCalories, topN } from './day1';
 
 describe('aoc day 1', () => {
   it('should return hello world', () => {
-    expect(hello()).toEqual('hello world');
+    expect(topN(1)).toEqual(65912);
   });
+
+  it('should return top three', () => {
+    expect(topN()).toEqual(195625);
+  })
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,3 +1,17 @@
-export function hello(): string {
-  return 'hello world';
-}
+import path from 'path';
+import fs from 'fs';
+
+export const topN = (n: number = 3) =>
+    fs.readFileSync(path.join(__dirname, 'input.txt')).toString().split('\n\n')
+      .reduce((calorieCount, calorieStr) =>
+          [
+            ...calorieCount,
+            calorieStr
+                .split('\n')
+                .map(cal => parseInt(cal, 10))
+                .reduce((acc, curr) => acc + curr, 0)
+          ], [] as number[])
+      .sort((a, b) => b - a)
+      .slice(0, n)
+      .reduce((acc, curr) => acc + curr);
+
