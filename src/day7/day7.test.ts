3ebe85a5e73ebea316938db2b9781462fbf8fd6a
Dilip Thomas <dthomas84@gmail.com>
2022-12-07 20:40:43 +1100

    publish day 7 solutions.

---
+++ src/day7/day7.test.ts
@@ -0,0 +1,11 @@
+import { part1, part2 } from './day7';
+
+describe('aoc day 7', () => {
+  it('should do part 1', () => {
+    expect(part1()).toEqual(1334506);
+  });
+
+  it('should do part 2', () => {
+    expect(part2()).toEqual(7421137);
+  });
+});
